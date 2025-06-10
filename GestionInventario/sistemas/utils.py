dd1ce27ebf2df0a99c55102c36c7a8b8bbf61c6a
Alejandra Codocedo <lettar.hyuga@gmail.com>
2025-06-10 01:47:24 -0400

    refactorizacion con pylint

--- GestionInventario/sistemas/utils.py
+++ GestionInventario/sistemas/utils.py
@@ -1,7 +1,13 @@
+"""
+funciones utilitarias para el sistema de inventario
+"""
 from django.db.models import Sum
+from .models import Stock
 
 def calcular_stock(juego):
-    from .models import Stock 
+    """
+    Calcula el stock total de un juego específico.
+    """
     return Stock.objects.filter(juego=juego).aggregate(
         total=Sum('cantidad')
     )['total'] or 0
