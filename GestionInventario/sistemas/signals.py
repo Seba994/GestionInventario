e975a4de83c01806f1a81e93ecd353ad70711e5c
Alejandra Codocedo <lettar.hyuga@gmail.com>
2025-06-10 01:48:23 -0400

    modificacion signals

--- GestionInventario/sistemas/signals.py
+++ GestionInventario/sistemas/signals.py
@@ -5,5 +5,4 @@ from .models import Juego, Stock
 @receiver(post_save, sender=Stock)
 def update_juego_stock(sender, instance, **kwargs):
     """Actualiza el stock total del juego cuando se modifica un stock"""
-    instance.juego.stock_total = instance.juego.stocks.aggregate(total=Sum('cantidad'))['total'] or 0
-    instance.juego.save(update_fields=['stock_total'])
+    pass
